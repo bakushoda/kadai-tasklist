e51629065f0ca5f248a4319fe4ad13e65724db19
baku.shoda <baku.shoda@icloud.com>
2020-09-02 04:50:33 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -45,12 +45,12 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            "title" => "required|max:255",
+            "status" => "required|max:10",
             "content" => "required|max:255",
             ]);
         
         $task = new Task;
-        $task->title = $request->title;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -97,12 +97,12 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $request->validate([
-            'title' => 'required|max:255',   
+            'status' => 'required|max:10',   
             'content' => 'required|max:255',
         ]);
         
         $task = Task::findOrFail($id);
-        
+        $message->status = $request->status; 
         $task->content = $request->content;
         $task->save();
         
